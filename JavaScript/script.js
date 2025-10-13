3ad75fdf16f40d9e2a47db5357ef3d1b1b05fe16
Mylan <mylan.baudin-marie@etu.unicaen.fr>
2025-10-14 00:22:28 +0200

    Carte dans canvas + modif CSS et HTML

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -1,1 +1,11 @@
+const canvas = document.getElementById('carte')
+const ctx = canvas.getContext('2d')
+
+let img = new Image()
+img.src = "../images/CarteAuTresor.jpg"
+
+img.onload = function(){
+    ctx.drawImage(img,0,0, canvas.width, canvas.height)
+}
+
 
