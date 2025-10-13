75284667dfa96977da440916ce627e26e73d29a2
Mylan <mylan.baudin-marie@etu.unicaen.fr>
2025-10-14 00:30:55 +0200

    Ajout clic canvas

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -8,4 +8,11 @@ img.onload = function(){
     ctx.drawImage(img,0,0, canvas.width, canvas.height)
 }
 
+let nbClickLeft = document.getElementById('nbClickLeft')
+let nbClick = 15
+nbClickLeft.innerHTML = `${nbClick} / 15`
 
+canvas.addEventListener('click', () =>{
+    nbClick = nbClick - 1
+    nbClickLeft.innerHTML = `${nbClick} / 15`
+})
