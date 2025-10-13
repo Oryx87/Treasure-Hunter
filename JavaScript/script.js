5b4bfcdeaed8e9836a73fd67759b5781631b01a4
Mylan <mylan.baudin-marie@etu.unicaen.fr>
2025-10-14 00:56:27 +0200

    Add tresor

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -9,8 +9,12 @@ img.onload = function(){
 }
 
 let nbClickLeft = document.getElementById('nbClickLeft')
+
 let nbClick = 15
 nbClickLeft.innerHTML = `${nbClick} / 15`
+
+let tresor = [Math.random() * canvas.width, Math.random() * canvas.height]
+console.log(tresor)
 
 canvas.addEventListener('click', () =>{
     nbClick = nbClick - 1
