716ec26b9ccddfa57a430ff61e5139cac422fe7d
Mylan <mylan.baudin-marie@etu.unicaen.fr>
2025-10-15 00:30:44 +0200

    Changement taille carte + suppression footer + ajout 15 clics max + trésor a trouvé

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -13,10 +13,44 @@ let nbClickLeft = document.getElementById('nbClickLeft')
 let nbClick = 15
 nbClickLeft.innerHTML = `${nbClick} / 15`
 
-let tresor = [Math.random() * canvas.width, Math.random() * canvas.height]
-console.log(tresor)
+let tresor = [Math.round(Math.random() * canvas.width), Math.round(Math.random() * canvas.height)]
+console.log("Position Tresor : " + tresor[0] + " " + tresor[1]) 
 
-canvas.addEventListener('click', () =>{
-    nbClick = nbClick - 1
-    nbClickLeft.innerHTML = `${nbClick} / 15`
-})
+
+function clickMap(){
+    canvas.addEventListener('click', (event) =>{
+        if (nbClick <= 0) return
+
+        nbClick = nbClick - 1
+        nbClickLeft.innerHTML = `${nbClick} / 15`
+
+        if (nbClick == 0){
+            alert("Vous avez échoué !")
+        }
+
+        const rect = canvas.getBoundingClientRect()
+        const x = event.clientX - rect.left
+        const y = event.clientY - rect.top
+        console.log("Position clic : " + x + " " + y)
+
+        const dx = x - tresor[0]
+        const dy = y - tresor[1]
+
+        distance = Math.sqrt(dx**2 + dy**2)
+        console.log(distance)
+
+        if (distance < 10){
+            alert("Bravo, vous avez trouvé le trésor !")
+        } else if (distance < 30 && distance > 10){
+            console.log("Brûlant !")
+        } else if (distance < 50 && distance > 30){
+            console.log("Chaud")
+        } else if (distance < 100 && distance > 50){
+            console.log("Tiède")
+        } else{
+            console.log("Glacé")
+        }
+    })
+}
+
+clickMap();
