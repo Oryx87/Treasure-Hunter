cd8f80e81570aee7fadecc87c1e977331fa29ade
Mylan <baudin.mylan@laposte.net>
2025-10-16 16:26:10 +0200

    modif img

--- JavaScript/script.js
+++ JavaScript/script.js
@@ -2,7 +2,7 @@ const canvas = document.getElementById('carte')
 const ctx = canvas.getContext('2d')
 
 let img = new Image()
-img.src = "../images/CarteAuTresor.jpg"
+img.src = "./images/CarteAuTresor.jpg"
 
 img.onload = function(){
     ctx.drawImage(img,0,0, canvas.width, canvas.height)
